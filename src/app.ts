13cdfdec19d3701c1945e1a024b4d4da37f35900
José Farley Sousa da Silva <202012010019@ad.ifpb.edu.br>
2023-11-22 15:28:41 -0300

    adicionando cors

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import express from 'express';
 import morgan from 'morgan';
 import { routes } from './routes/routes';
 import path from 'path'
+import cors from 'cors'
 
 const app = express();
 app.use(express.static(__dirname + "/images"));
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 export {app}
